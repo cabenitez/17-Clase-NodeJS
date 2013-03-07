474585d05e56ed71c245f8820d0761520b70f6e3
Siedrix <Siedrix@gmail.com>
2013-03-06 19:31:32 -0500

    Archivos de la clase

---
+++ grid/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = require('socket.io').listen(server);
+
+var cons = require('consolidate');
+
+server.listen(3000);
+
+app.engine('.html', cons.jade);
+app.set('view engine', 'html');
+
+app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+	res.render('index',{
+		titulo : 'Applicacion Grip'
+	});
+});
+
+var connection = function(socket){
+	console.log('hola');
+
+	socket.on('pintar', function(data){
+		socket.broadcast.emit('pintar', data);
+	})
+}
+
+io.sockets.on('connection', connection)
